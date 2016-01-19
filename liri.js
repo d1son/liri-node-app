0b51e1b41c3bcdcc5de60218c0c09bc0f9439194
d1son <dson120@gmail.com>
2016-01-19 18:52:41 -0500

    added omdb title search

--- liri.js
+++ liri.js
@@ -1,12 +1,19 @@
 var fs = require("fs");
-var keys = require("keys");
-var twitter = require('twitter');
-var spotify = require("spotify");
+// var keys = require("keys");
+// var twitter = require('twitter');
+// var spotify = require("spotify");
 var request = require('request');
 
 var params = process.argv.slice(2);
 
 
-fs.readFile("keys.js", "utf8", function(error, data){
+request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage. 
+  }
+})
 
-});
+
+// fs.readFile("keys.js", "utf8", function(error, data){
+
+// });
