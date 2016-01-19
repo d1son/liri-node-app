e93f854b5b8a2e6c77c013291105d86f1798bcc8
d1son <dson120@gmail.com>
2016-01-19 16:03:46 -0500

    added global variables

---
+++ liri.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+var keys = require("keys");
+var twitter = require('twitter');
+var spotify = require("spotify");
+var request = require('request');
+
+var params = process.argv.slice(2);
+
+
+fs.readFile("keys.js", "utf8", function(error, data){
+
+});
