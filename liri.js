24fddbaade8b2adcd47e10902c4c28a3b48bda5d
d1son <dson120@gmail.com>
2016-01-19 19:58:58 -0500

    added search movie function

--- liri.js
+++ liri.js
@@ -2,16 +2,54 @@ var fs = require("fs");
 // var keys = require("keys");
 // var twitter = require('twitter');
 // var spotify = require("spotify");
-var request = require('request');
+var request = require("request");
 
 var params = process.argv.slice(2);
 
+switch(params[0]){
+	case "movie-this":
+	getMovie();
+	break;
 
-request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
-  }
-})
+	default:
+		console.log("err");
+}
+
+function getMovie(){
+	if(params[1] === undefined){
+		request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
+		  if (!error && response.statusCode == 200){
+		  var movieInfo = "Title: " + JSON.parse(body)["Title"] + "\r\n" + 
+		  								"Year: " + JSON.parse(body)["Year"] + "\r\n" + 
+		  								"IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +  
+		  								"Country: " + JSON.parse(body)["Country"] + "\r\n" + 
+		  								"Language: " + JSON.parse(body)["Language"] + "\r\n" + 
+		  								"Plot: " + JSON.parse(body)["Plot"] + "\r\n" + 
+		  								"Actors: " + JSON.parse(body)["Actors"] + "\r\n" + 
+		  								"Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" + 
+		  								"Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"];
+			console.log(movieInfo);
+			return;
+		}
+	})
+} else {
+	request('http://www.omdbapi.com/?t=' + params[1] + '&plot=short&r=json&tomatoes=true', function (error,response, body){
+		if (!error && response.statusCode == 200){
+			var movieInfo = "Title: " + JSON.parse(body)["Title"] + "\r\n" + 
+		  								"Year: " + JSON.parse(body)["Year"] + "\r\n" + 
+		  								"IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +  
+		  								"Country: " + JSON.parse(body)["Country"] + "\r\n" + 
+		  								"Language: " + JSON.parse(body)["Language"] + "\r\n" + 
+		  								"Plot: " + JSON.parse(body)["Plot"] + "\r\n" + 
+		  								"Actors: " + JSON.parse(body)["Actors"] + "\r\n" + 
+		  								"Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" + 
+		  								"Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"];
+		  console.log(movieInfo);
+		  return;
+			}
+		})
+	}
+} 
 
 
 // fs.readFile("keys.js", "utf8", function(error, data){
